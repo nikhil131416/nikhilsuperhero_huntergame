40e30718c8c75b613a223d9e0e9fb1a609ce543b
Pankaj <pankajbeniwal3112@gmail.com>
2023-06-20 19:28:34 +0530

    initial commit

---
+++ api.js
@@ -0,0 +1,19 @@
+// Account 1
+let timeStamp = "1686911318516";
+let publicKey = "eb381db2398bd81e7738d7ca425f0cd2";
+let privateKey = "fec7ec85fed58c07a6d492cd9caf51f177a95b2e";
+let Md5hash = "2691e0b093c73db20520302cabf72105";
+
+
+//Account 2
+// let timeStamp = "1687188539304";
+// let publicKey = "5de4c322cc453ad2ed68452cb37ca497";
+// let privateKey = "a62d6d6c4b675aecceabf49bd36df302bbd8583e";
+// let Md5hash = "238c5fe2a4adb7c32f3084973896a890";
+
+
+// Account 3
+// let timeStamp = "1686911318516";
+// let publicKey = "1e00168d6c2fb1008542fcccd740912d";
+// let privateKey = "a39a6cf8e24851574ddf2995a6e9ae6effa9f52d";
+// let Md5hash = "96bd4d69ceef2d39c081865ce3590815";
